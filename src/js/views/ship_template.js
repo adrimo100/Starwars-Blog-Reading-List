68bf0edc50a8a48d982a7b4178ee7a39eeafe19d
Adrián Molina Elvira <adrimo100@gmail.com>
2022-02-22 18:55:21 +0000

    primera versión

---
+++ src/js/views/ship_template.js
@@ -0,0 +1,100 @@
+import React, {useState, useEffect} from "react";
+import propTypes from "prop-types";
+
+import "../../styles/ship_template.css"
+
+export const Ship = (props) => {
+
+    const [characterData, setCharacterData] = useState(null);
+
+    useEffect(async () => {
+
+        await fetch(props.shipURL)
+        .then(response => {
+                if(!response.ok)
+                    throw new Error("Error when loading character data")
+                
+                return response.json();
+        })
+        .then(data =>{
+            setCharacterData(data);
+            
+            console.log(data);
+
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+        await fetch(props.imageURL)
+        .then(response => {
+                if(!response.ok)
+                    throw new Error("Error when loading character image")
+                
+                return response.json();
+        })
+        .then(data =>{
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+
+
+    }, [])
+
+    return (
+        <div>
+            <div className="row">
+                
+                <div className="col-md-12 col-xl-5 ml-3 text-center mb-4">
+
+                    <img id="shipImg" src=""/>
+
+                </div>
+                <div className="col-md-12 col-xl-6 ml-3 text-center mb-4">
+                    <h2>{characterData ? characterData.result.properties.name : ""}</h2>
+                    <p>{characterData ? characterData.result.description : ""}</p>
+
+                </div>
+               
+            </div>
+            <div className="row p-5 text-danger">
+                 <hr />
+                 <div className="col-xl-2 col-sm-12 mt-1">
+                    <strong>NAME</strong>
+                    <p>{characterData ? characterData.result.properties.name : ""}</p>
+                </div>
+                <div className="col-xl-2 col-sm-12 mt-1">
+                <strong>MODEL</strong>
+                    <p>{characterData ? characterData.result.properties.model : ""}</p>
+                </div>
+                <div className="col-xl-2 col-sm-12 col-sm-12 mt-1">
+                <strong>MANUFACTURER</strong>
+                    <p>{characterData ? characterData.result.properties.manufacturer : ""}</p>
+                </div>
+                <div className="col-xl-2 col-sm-12 mt-1">
+                <strong>CREW</strong>
+                    <p>{characterData ? characterData.result.properties.crew : ""}</p>
+                </div>
+                <div className="col-xl-2 col-sm-12 mt-1">
+                <strong>LENGTH</strong>
+                    <p>{characterData ? characterData.result.properties.length : ""}</p>
+                </div>
+                <div className="col-xl-2 col-sm-12 mt-1">
+                <strong>HYPERDRIVE RATING</strong>
+                    <p>{characterData ? characterData.result.properties.hyperdrive_rating : ""}</p>
+                </div>
+            </div>
+           
+        </div>
+    )
+
+}
+
+Ship.propTypes = {
+
+    shipURL: propTypes.string,
+    imageURL: propTypes.string,
+}
