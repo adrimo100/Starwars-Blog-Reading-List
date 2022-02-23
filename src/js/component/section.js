0e9dab1c8e1b5cf8b097771a89a408b84c0eb3a8
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-02-23 22:20:09 +0100

    Try to move fetching logic to Flux

--- src/js/component/section.js
+++ src/js/component/section.js
@@ -1,28 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Card from "./card";
 import "../../styles/section.css";
+import { Context } from "../store/appContext";
 
 const Section = ({ resourcePath, title }) => {
-  const [resource, setResource] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://www.swapi.tech/api/${resourcePath}/`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setResource(data.results);
-      })
-      .catch((err) => console.error(err));
-  }, []);
+  const { store } = useContext(Context);
 
   return (
     <section className="resource">
       <h2 className="text-danger mb-3"> {title} </h2>
 
       <div className="d-flex gap-4 cards-container">
-        {resource.map((element) => (
-          <Card {...element} resourcePath={resourcePath} key={element.uid} />
-        ))}
+        {store.resources[resourcePath] &&
+          store.resources[resourcePath].map((element) => (
+            <Card {...element} resourcePath={resourcePath} key={element.uid} />
+          ))}
       </div>
     </section>
   );
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,70 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+  return {
+    store: {
+      resources: {
+        people: [],
+        starships: [],
+        planets: [],
+      },
+      favourites: [],
+    },
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+    actions: {
+      addFavourite: (name) => {
+        const favourites = getStore().resources.favourites;
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+        setStore({ favourites: [...favourites, name] });
+      },
+      removeFavourite: (name) => {
+        const favourites = getStore().resources.favourites;
+
+        setStore({
+          favourites: favourites.filter((elem) => elem.name !== name),
+        });
+      },
+
+      getAll: () => {
+        const resources = getStore().resources;
+
+        let newResources = {};
+
+        Object.keys(resources).forEach((resource) => {
+          fetch(`https://www.swapi.tech/api/${resource}/`)
+            .then((res) => res.json())
+            .then((data) => {
+              newResources[resource] = data.results;
+            })
+            .catch((err) => console.error(err));
+        });
+
+        setStore({ resources: newResources });
+      },
+
+      // // Use getActions to call a function within a fuction
+      // exampleFunction: () => {
+      //   getActions().changeColor(0, "green");
+      // },
+      // loadSomeData: () => {
+      //   /**
+      // 		fetch().then().then(data => setStore({ "foo": data.bar }))
+      // 	*/
+      // },
+      // changeColor: (index, color) => {
+      //   //get the store
+      //   const store = getStore();
+
+      //   //we have to loop the entire demo array to look for the respective index
+      //   //and change its color
+      //   const demo = store.demo.map((elm, i) => {
+      //     if (i === index) elm.background = color;
+      //     return elm;
+      //   });
+
+      //   //reset the global store
+      //   setStore({ demo: demo });
+      // },
+    },
+  };
 };
 
 export default getState;
